b8059936fb891fff9b062f961058f433186b9193
Nikita <nicitazaletin@yandex.ru>
2020-10-18 21:28:45 +0300

    practice10

---
+++ practice10-Zaletin/src/Complex.java
@@ -0,0 +1,32 @@
+public class Complex {
+    private int real;
+    private int image;
+
+    Complex(){}
+
+    Complex(int real, int image){
+        this.real = real;
+        this.image = image;
+    }
+
+    public int getReal() {
+        return real;
+    }
+
+    public void setReal(int real) {
+        this.real = real;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return real + "+" + image + "i";
+    }
+}
---
+++ practice10-Zaletin/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Complex cmp1 = new Complex(0,1);
+        Complex cmp2 = new ConcreteFactory().createComplex();
+        cmp2.setImage(1);
+        cmp2.setReal(1);
+        System.out.println(cmp1);
+        System.out.println(cmp2);
+
+    }
+}
